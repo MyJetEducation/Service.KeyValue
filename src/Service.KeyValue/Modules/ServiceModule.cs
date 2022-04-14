070847d447b5c5395a5d5365eb9d8f733acb5d8a
dmitriy.b <dmitriy.b@smplt.net>
2022-04-14 14:31:49 +0500

    DI fixed

--- src/Service.KeyValue/Modules/ServiceModule.cs
+++ src/Service.KeyValue/Modules/ServiceModule.cs
@@ -2,6 +2,7 @@
 using MyJetWallet.Sdk.ServiceBus;
 using MyServiceBus.Abstractions;
 using MyServiceBus.TcpClient;
+using Service.KeyValue.Jobs;
 using Service.KeyValue.Postgres.Services;
 using Service.ServiceBus.Models;
 
@@ -17,6 +18,8 @@ namespace Service.KeyValue.Modules
 
 			MyServiceBusTcpClient serviceBusClient = builder.RegisterMyServiceBusTcpClient(Program.ReloadedSettings(e => e.ServiceBusReader), Program.LogFactory);
 			builder.RegisterMyServiceBusSubscriberBatch<ClearEducationUiProgressServiceBusModel>(serviceBusClient, ClearEducationUiProgressServiceBusModel.TopicName, QueueName, TopicQueueType.Permanent);
+
+			builder.RegisterType<ClearEducationUiProgressNotificator>().AutoActivate().SingleInstance();
 		}
 	}
 }
